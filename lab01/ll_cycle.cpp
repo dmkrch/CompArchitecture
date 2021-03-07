00ec43399e6efd3660f99c0e1ba02bc77b00cec8
Dmitriy <dmkrch1@gmail.com>
2021-03-07 20:15:55 +0300

    task 4,5,6 done

--- lab01/ll_cycle.cpp
+++ lab01/ll_cycle.cpp
@@ -1,6 +1,35 @@
 #include "ll_cycle.h"
 
-bool ll_has_cycle(node *head) {
-    /* Ваш код должен быть написан только внутри этой функции */
+bool ll_has_cycle(node *head) 
+{   
+    // checking if head is not null
+    if (head == nullptr)
+        return false;
+    
+    // setting our 'animals' so that they point to head
+    node* turtle = head;
+    node* rabbit = head;
+
+    while(true)
+    {
+        // cycle for moving rabbit 2 nodes forward
+        for (int i = 0; i < 2; ++i)
+        {
+            // if next is null - then list is not cycled
+            if (rabbit->next == nullptr)
+                return false;
+
+            // moving rabbit forward for a node
+            rabbit = rabbit->next;
+        }
+
+        // moving turtle forward for a node
+        turtle = turtle->next;
+
+        // if our animals met together - list is cycled
+        if (rabbit == turtle)
+            return true;
+    }
+
     return true;
 }
